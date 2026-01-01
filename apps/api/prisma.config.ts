1696d75523c28e491deb05259c42a4030a22d562
Mohamad Alkhatib <mohammad.alkhatib@web.de>
2026-01-01 02:17:47 +0100

    Fixed prisma issues

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "./src/prisma/schema.prisma",
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
 });
